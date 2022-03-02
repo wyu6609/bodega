8abbb7598b2bab74f50c4e47a5ccd92dad65fd8a
William Yu <wy427@nyu.edu>
2022-03-02 17:43:55 -0500

    Update CurrentInventoryList.js

--- src/components/CurrentInventoryList.js
+++ src/components/CurrentInventoryList.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
+import InventoryItemCard from "./InventoryItemCard";
+import { v4 as uuid } from "uuid";
 
-function CurrentInventoryList() {
-
-    return(
-        <div id="current-inventory">
-            <h2>Current Inventory</h2>
-            <div>
-                {/** Put inventory items here */}
-            </div>
-        </div>
-    );
+function CurrentInventoryList({ currentItems, addItem, deleteHandler }) {
+  return (
+    <div id="current-inventory">
+      <h2>Current Inventory</h2>
+      <div>
+        {currentItems.map((item) => {
+          return (
+            <InventoryItemCard
+              key={uuid()}
+              item={item}
+              addItem={addItem}
+              deleteHandler={deleteHandler}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default CurrentInventoryList;
